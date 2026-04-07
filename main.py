803b44956491013085eb775666f575f69a9b5c1f
jardevesterinen-svg <jarde.vesterinen@gmail.com>
2026-04-07 07:52:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -355,9 +355,9 @@ async def generate_report(kohde_id: str):
         pdf.drawImage(
             glacier,
             0,
-            h - HEADER_HEIGHT,     # slightly above stone header overlap
-            width=220,
-            height=HEADER_HEIGHT,
+            h - HEADER_HEIGHT + 6,     # slightly above stone header overlap
+            width=150
+            height=HEADER_HEIGHT + 6,
             mask="auto"
         )
     except:
