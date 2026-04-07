765687f77e7255c8a78d0159317a354c17080962
jardevesterinen-svg <jarde.vesterinen@gmail.com>
2026-04-07 09:14:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -370,7 +370,7 @@ async def generate_report(kohde_id: str):
             logo,
             30,
         h - HEADER_HEIGHT + 8,   # ✅ sisällä, EI yläpuolella
-        width=110,
+        height=HEADER_HEIGHT - 12
         preserveAspectRatio=True,
         mask="auto"
         )
