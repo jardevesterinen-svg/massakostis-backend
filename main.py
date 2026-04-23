037db0ebe015256831cf68e00c5e0a78324080c1
jardevesterinen-svg <jarde.vesterinen@gmail.com>
2026-04-23 11:48:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -358,7 +358,7 @@ async def generate_report(kohde_id: str):
     HEADER_BOTTOM_Y = h - HEADER_HEIGHT
          
     IMAGES_TOP_Y = HEADER_BOTTOM_Y - 15
-    MATERIALS_TOP_Y = IMAGES_TOP_Y - IMAGES_MAX_HEIGHT - 15
+    MATERIALS_TOP_Y = IMAGES_TOP_Y - IMAGES_MAX_HEIGHT
     
     # ======================================================
     # =============  PAGE 1 — KANSILEHTI  ==================
