154d0abca5d6c3b65c0e526b8cb4a6f0b794f69b
jardevesterinen-svg <jarde.vesterinen@gmail.com>
2026-08-04 19:57:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -571,6 +571,7 @@ async def generate_report(kohde_id: str):
             ("lattiakallistukset", "Lattiakallistukset"),
             ("erillis-wc", "Erillis-WC"),
             ("sauna", "Sauna")
+            ("keittiö", "Keittiö")
         ]
     
         # ---- LOAD HUONEISTOT ----
